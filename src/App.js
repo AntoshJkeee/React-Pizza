d08c18aef8328db8ff7c41a4cab3de24e6b99425
antoshjkeee <toha_bart@mail.ru>
2022-01-20 00:39:37 +0300

    компонент пиццы

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { Header } from './components'
+import {Header} from './components'
 import {Cart, Home} from './pages'
 import {Route, Routes} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-      <div className="wrapper">
-          <Header />
-          <div className="content">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/cart" element={<Cart />} />
-              </Routes>
-          </div>
-      </div>
-  );
+    const [pizzas, setPizzas] = useState([])
+
+
+    useEffect(() => {
+        fetch('http://localhost:3000/db.json').then(response => response.json()).then(response => {
+            setPizzas(response.pizzas)
+        })
+    }, [])
+
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home items={pizzas}/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
